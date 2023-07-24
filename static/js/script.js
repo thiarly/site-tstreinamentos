8277522dcedabf6ae15fe8a562ca72d83adf4359
Thiarly Cavalcante <thiarly.cavalcante@live.com>
2023-07-24 14:21:54 -0300

    deploy site ts treinamentos

---
+++ static/js/script.js
@@ -0,0 +1,8 @@
+const btNav = document.querySelector('#btNav');
+const divNav = document.querySelector('#divNav');
+
+btNav.addEventListener('click', function() {
+    divNav.classList.toggle('hidden');
+})
+
+
